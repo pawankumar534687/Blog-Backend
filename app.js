a18954640796d721ff57eb39fa237b0403cbba97
pawankumar534687 <pawankumar534687@gmail.com>
2025-07-30 18:23:55 -1200

    updated

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ dotenv.config()
 
 const PORT = process.env.PORT || 6000
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174' ], 
+    origin: ['http://localhost:5173', 'http://localhost:5174','https://blog-frontend-388e.onrender.com','https://blog-admin-zu4c.onrender.com' ], 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
    
 }));
